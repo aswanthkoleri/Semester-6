a378599efbfd5de1636f195d76f44001b98ac278
aswanthkoleri <aswanth9495@gmail.com>
2019-04-12 15:34:42 +0530

    Add some small changes in the output of lab5 code(IDMW)

--- IDM/Lab/lab5/k_medoids.py
+++ IDM/Lab/lab5/k_medoids.py
@@ -6,14 +6,15 @@ data = pd.read_excel('Absenteeism_at_work.xls')
 unused_attributes = ['ID']
 data = data.drop(unused_attributes, axis = 1)
 points = []
+print(data)
 points = np.array(data)
-
+print(points)
 class k_medoids:
     def __init__(self, points, k):
         self.medoids = []
         self.k = k
         self.points = points
-        
+
         for i in range(k):
             index = int((i/float(self.k)) * points.shape[0])
             self.medoids.append(points[index])
@@ -34,21 +35,21 @@ class k_medoids:
             minIndex = distances.index(min(distances))
 
             clusters.setdefault(minIndex, []).append(data_point)
-            
+
         return clusters
 
     def find_medoids(self, cluster):
         min_distance = 100000000
         for point in cluster:
             temp = 0
-            
+
             for compare_point in cluster:
                 temp += self.distance(point, compare_point)
-            
+
             if temp < min_distance:
                 min_distance = temp
                 medoid = point
-                    
+
         return medoid
 
     def fit(self):
@@ -56,14 +57,18 @@ class k_medoids:
         self.k_clusters = {}
         while (np.array_equal(current_medoids, self.medoids) == False):
             self.k_clusters = self.find_clusters()
-            
+
             for i in range(self.k):
                 current_medoids[i] = self.medoids[i]
                 medoid = self.find_medoids(self.k_clusters[i])
                 self.medoids[i] = medoid
-            
+
         for i in range(len(self.k_clusters)):
-            print(np.array(self.k_clusters[i]).shape)
+            print("# Cluster ",i+1)
+            # print(np.array(self.k_clusters[i]).shape)
+            item=np.array(self.k_clusters[i]).shape
+            print("No of points : ",item[0])
+            print("No of Columns : ",item[1])
 
 model = k_medoids(points, 2)
 
